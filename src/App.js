4d7fa6b5d0c346baa5c9b3fc321684a9f89d3bca
Nuutti <nuutti.niinivaara@tuni.fi>
2026-03-13 17:45:03 +0200

    files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
+import img_1 from './IMG_8106.png';
+import img_2 from './img_2.png';
+import img_3 from './img_3.png';
+import img_4 from './img_4.png';
+import img_5 from './img_5.png';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='App-div'>
+          <img src={img_1} className="App-logo" alt="logo" />
+          <img src={img_2} className="App-logo" alt="logo" />
+          <img src={img_3} className="App-logo" alt="logo" />
+          <img src={img_4} className="App-logo" alt="logo" />
+          <img src={img_5} className="App-logo" alt="logo" />
+        </div>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Moi mä rakastan sua!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
